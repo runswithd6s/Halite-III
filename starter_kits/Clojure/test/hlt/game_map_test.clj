27e110b253a51d3e8f7f5ab9613e4bc8436a3c15
Chad Walstrom <chewie@wookimus.net>
2018-12-01 15:41:32 -0600

    [starter_kit/Clojure] Tests added for functions

--- starter_kits/Clojure/test/hlt/game_map_test.clj
+++ starter_kits/Clojure/test/hlt/game_map_test.clj
@@ -3,6 +3,7 @@
             [hlt.ship :as ship]
             [hlt.player :as player]
             [hlt.shipyard :as shipyard]
+            [hlt.direction :as direction]
             [hlt.game-map :refer :all]))
 
 (deftest test-naive-navigate
@@ -10,3 +11,17 @@
   (let [ship {::ship/id 1 ::player/id 0 :position [8 10] :halite 100}
         shipyard {::shipyard/id -1  ::player/id 0 :position [8 16]}]
     (is (= [0 1] (naive-navigate ship shipyard))))))
+
+(deftest test-get-target-direction
+  (with-redefs [game-map (fn [] {:width 32 :height 32})]
+    ;; one direction -- not shortest
+    (is (= (get-target-direction [0 0] [0 32]) [nil direction/south]))
+    (is (= (get-target-direction [0 0] [0 1]) [nil direction/south]))
+    (is (= (get-target-direction [0 0] [1 0]) [direction/east nil]))
+    (is (= (get-target-direction [0 0] [32 0]) [direction/east nil]))
+    ;; two directions
+    (is (= (get-target-direction [1 1] [0 2]) [direction/west direction/south]))
+    (is (= (get-target-direction [1 1] [0 0]) [direction/west direction/north]))
+    (is (= (get-target-direction [1 1] [2 2]) [direction/east direction/south]))
+    (is (= (get-target-direction [1 1] [2 0]) [direction/east direction/north]))
+    ))
